a11dedbe41a7d585ad1a6361d42b88be6f9df513
David Muniak <godai@centaurstage.com>
2012-05-17 23:36:38 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitRail::Application.config.secret_token = '1713d153d809e37a585da74565ed94ee3f9249dc1babbf1d18389699e847f37dd1d90adc05d215f9bd15ad7a5e39d9a02d294c7b6773ffd0acc255b613bb2fc5'
